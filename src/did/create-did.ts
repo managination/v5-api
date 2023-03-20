a94099b63c137a6134ce740b406b9b01405fc029
Micha Roon <micha.roon@managination.com>
2023-03-20 23:05:24 +0100

    local tests and createWeb3Name

--- src/did/create-did.ts
+++ src/did/create-did.ts
@@ -1,8 +1,18 @@
 import * as Kilt from '@kiltprotocol/sdk-js'
+import {
+  Blockchain,
+  Did,
+  DidResourceUri,
+  ISubmittableResult,
+  KiltKeyringPair,
+  SignRequestData,
+  SubmittableExtrinsic
+} from '@kiltprotocol/sdk-js'
 import { mnemonicGenerate } from "@polkadot/util-crypto";
 import { generateKeypairs, submitterAccount } from "../utilities";
+import BN from "bn.js";
 
-export async function createDid() : Promise<{
+export async function createDid(): Promise<{
   mnemonic: string
   fullDid: Kilt.DidDocument
 }> {
@@ -17,16 +27,16 @@ export async function createDid() : Promise<{
   // Get tx that will create the DID on chain and DID-URI that can be used to resolve the DID Document.
   const fullDidCreationTx = await Kilt.Did.getStoreTx(
       {
-        authentication: [authentication],
-        keyAgreement: [keyAgreement],
-        assertionMethod: [assertionMethod],
-        capabilityDelegation: [capabilityDelegation]
+        authentication: [ authentication ],
+        keyAgreement: [ keyAgreement ],
+        assertionMethod: [ assertionMethod ],
+        capabilityDelegation: [ capabilityDelegation ]
       },
       submitterAccount().address,
-      async ({ data }) => ({
+      async ({ data }) => ( {
         signature: authentication.sign(data),
         keyType: authentication.type
-      })
+      } )
   )
 
   await Kilt.Blockchain.signAndSubmitTx(fullDidCreationTx, submitterAccount())
@@ -41,4 +51,51 @@ export async function createDid() : Promise<{
 
   return { mnemonic, fullDid: document }
 
+}
+
+export async function createW3n(
+    didMnemonic: string,
+    web3Name: string
+) {
+  const api = Kilt.ConfigService.get('api')
+  const account: Kilt.KiltKeyringPair = submitterAccount()
+  const { authentication: didAuth } = generateKeypairs(didMnemonic)
+  const didUri = Kilt.Did.getFullDidUriFromKey(didAuth)
+
+  const authorized = await Did.authorizeTx(
+      didUri,
+      api.tx.web3Names.claim(web3Name),
+      async (signRequest: SignRequestData) => {
+        const signature = didAuth.sign(signRequest.data, { withType: false });
+        const keyUri = `${didUri}:${didAuth.address}` as DidResourceUri;
+        const keyType = didAuth.type;
+
+        return {
+          signature,
+          keyUri,
+          keyType,
+        };
+      },
+      account.address
+  );
+  const tx = await submitTx(account, authorized);
+  await tx.finalizedPromise
+}
+
+export async function submitTx(
+    keypair: KiltKeyringPair,
+    draft: SubmittableExtrinsic,
+    tip = new BN(0),
+): Promise<{
+  txHash: string;
+  finalizedPromise: Promise<ISubmittableResult>;
+}> {
+  const extrinsic = await draft.signAsync(keypair, { tip });
+  const txHash = extrinsic.hash.toHex();
+  const finalizedPromise = Blockchain.submitSignedTx(extrinsic);
+
+  return {
+    txHash,
+    finalizedPromise,
+  };
 }
--- test/get-did.spec.ts
+++ test/get-did.spec.ts
@@ -1,22 +1,60 @@
 import dotenv from "dotenv";
 import { cryptoWaitReady } from "@polkadot/util-crypto";
-import * as Kilt from "@kiltprotocol/sdk-js";
-import { getDidByWeb3Name } from "../src/did/read-did";
+import { Crypto } from '@kiltprotocol/utils'
+import { getDidByWeb3Name, getDidDocument } from "../src/did/read-did";
+import { createDid, createW3n } from "../src/did/create-did";
+import { Blockchain, connect, DidDocument, disconnect } from "@kiltprotocol/sdk-js";
+import { submitterAccount } from "../src/utilities";
+import { randomUUID } from "crypto";
 
-dotenv.config();
+dotenv.config({path: "./.env-test"});
 
-describe("read DID from chain", function () {
+describe("create and read DID", function () {
+  jest.setTimeout(300000);
+
   beforeAll(async () => {
     await cryptoWaitReady();
-    await Kilt.connect(process.env.WSS_ADDRESS as string);
+    const api = await connect(process.env.WSS_ADDRESS as string);
+    const faucetSeed = "receive clutch item involve chaos clutch furnace arrest claw isolate okay together"
+    // creates an ed25519 key by default
+    const devFaucet = Crypto.makeKeypairFromUri(faucetSeed)
+    const account = submitterAccount()
+    const transferTx = api.tx.balances.transfer(account.address, "10000000000000000")
+    await Blockchain.signAndSubmitTx(transferTx, devFaucet)
   })
 
   afterAll(async () => {
-    await Kilt.disconnect()
+    await disconnect()
   })
 
-  it("reads an existing DID document by web3 name", async function () {
-    const didDocument = await getDidByWeb3Name("verifive");
-    expect(didDocument.web3Name).toEqual("verifive");
+  it("allows any user to create a DID", async function() {
+    const account = await createDid()
+    const did = await getDidDocument(account.fullDid.uri)
+    expect(account.fullDid.authentication).toEqual(did.document?.authentication)
+  })
+
+  describe("web3 names", function (){
+    var web3Name: string
+    var account: {mnemonic: string, fullDid: DidDocument}
+
+    beforeEach(async function() {
+      account = await createDid()
+      web3Name = randomUUID().substring(1, 32)
+    })
+
+    it.only("assigns a web3 name to a DID", async function() {
+      await createW3n(account.mnemonic, web3Name)
+      const didDocument = await getDidByWeb3Name(web3Name);
+      expect(didDocument.web3Name).toEqual(web3Name);
+    })
+
+    it("fails to read a non existing web3 name DID document", async function () {
+      try {
+        await getDidByWeb3Name(web3Name);
+        throw new Error("getWeb3Name should have thrown")
+      } catch (err: any) {
+        expect(err.message).toEqual(`web3Name ${web3Name} is not assigned`)
+      }
+    })
   })
 })
